1b5bb699cc21993bd23461ad652db04f16aa7f3f
DennisManyagi <39723205+DennisManyagi@users.noreply.github.com>
2018-06-12 14:54:14 -0700

    Add files via upload

---
+++ Manyagi_Dennis_json/JS/json_weather.js
@@ -0,0 +1,7 @@
+
+//JSON weather forecast
+var weather = { "Forecast" : [
+    { "City":"Orlando" , "Country":"United States" , "Wind": 76 , "Direction":360, "Humidity":48 , "Pressure":"29.65", "Date": 10-17-2014, "Day":"Friday", "High":80, "Low":62, "text": "Sunny" },
+    { "City":"Orlando" , "Country":"United States" , "Wind": 76 , "Direction":360, "Humidity":48 , "Pressure":"29.65", "Date": 10-18-2014, "Day":"Saturday", "High":82, "Low":63, "text": "Partly Cloudy "},
+    { "City":"Orlando" , "Country":"United States" , "Wind": 76 , "Direction":360, "Humidity":48 , "Pressure":"29.65", "Date": 10-9-2014, "Day":"Sunday", "High":85, "Low":65, "text": "Partly Cloudy"}]}
+
