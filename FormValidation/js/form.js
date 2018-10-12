cf647f16d1c33fc2185fdaf84c5e5cd690362d4f
Dennis Manyagi <manyagi.dennis@gmail.com>
2018-10-12 14:41:34 -0700

    all style changes completed

--- FormValidation/js/form.js
+++ FormValidation/js/form.js
@@ -11,3 +11,8 @@ console.log(background);
 
 background.style.backgroundImage = "url('https://gallery.mailchimp.com/217bb4cb236a854f10f4f13b9/images/079db29a-a9d9-455f-a266-fe37995aba90.jpg')";
 
+//change general information
+
+var info = document.querySelector('p');
+
+info.innerHTML = 'Stream and buy official anime including My Hero Academia, Drifters and Fairy Tail. Watch free anime online or subscribe for more. Start your free trial today.';
