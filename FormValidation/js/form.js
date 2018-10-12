b3429d399c942179cf247dccaa3a866146fe5f6c
Dennis Manyagi <manyagi.dennis@gmail.com>
2018-10-12 14:26:05 -0700

    title and background complete

---
+++ FormValidation/js/form.js
@@ -0,0 +1,13 @@
+//change title
+var title = document.querySelector('h2');
+
+title.innerHTML = 'Watch Anime Sign Up';
+
+//change the background image
+
+var background = document.querySelector('body');
+
+console.log(background);
+
+background.style.backgroundImage = "url('https://gallery.mailchimp.com/217bb4cb236a854f10f4f13b9/images/079db29a-a9d9-455f-a266-fe37995aba90.jpg')";
+
