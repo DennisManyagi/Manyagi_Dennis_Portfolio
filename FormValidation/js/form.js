d6cb79e7e74679cc6d81401095bb60dc4b7a0646
Dennis Manyagi <manyagi.dennis@gmail.com>
2018-10-12 16:01:15 -0700

    input validation working

--- FormValidation/js/form.js
+++ FormValidation/js/form.js
@@ -28,12 +28,106 @@ button.disabled = true;
 //enable button if all validation checks are approved
 //create validation errors for every single input
 //validation must display an error message on the inputs if not filled in
+//validate inputs
+function  validateForm(event) {
+
+    event.preventDefault(); //stop button submission
 
 
 
+    var form = document.querySelector('form');
+
+    var fields = form.querySelectorAll('input');
+
+
+//Check for valid values
+    var valid = true;
+
+    for (var i =0; i < fields.length; i++){
+
+        //check if fields have value, if not set to false
+        if (!fields[i].value){
+
+            valid = false;
+        }
+
+
+        //open submit
+        if (valid == true){
+
+            var submit = form.querySelector('[type=submit]');
+            submit.removeAttribute('disabled');
+        }
+    }
+
+
+
+}
+
+
+function validateRequired(event){
+
+
+    var target = event.target;
+    var parent = target.parentElement;
+    var error = '<label class="error">This field is required!</label>';
+
+
+    if (!target.value.length){
+
+        if (!parent.querySelector('.error')){
+
+            parent.insertAdjacentHTML('beforeend', error);
+
+        }
+
+    } else {
+
+        parent.removeChild(parent.querySelector('.error'));
+    }
+}
+
+//check if submissions are allowed
+var requiredFields = document.querySelectorAll('input');
+
+for (var i = 0; 0 < requiredFields.length ; i++){
+
+
+    requiredFields[i].addEventListener('input', validateForm);
+    requiredFields[i].addEventListener('blur', validateRequired);
+}
 
 
 
 
 //create a thank you screen
 //replace the form content only if validation passes
+
+//form submission
+
+function send(event){
+
+    event.preventDefault();//stop submission
+
+    var form = document.querySelector('form');
+
+    var message = '<h2>Thank you!</h2><p>You are so awesome, sharks love you!</p>';
+
+
+
+    var target = event.target;
+
+    var disabled = target.classList.contains('disabled');
+
+    if (disabled == false){
+
+
+        form.innerHTML = message;
+    }
+}
+
+//submit form
+
+var submit = document.querySelector('[type=submit]');
+
+submit.addEventListener('click', send);
